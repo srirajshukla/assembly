40159272c4773bd875a44d0df79516eb8b2d9f4a
Gyanendra Shukla <srirajshukla@gmail.com>
2020-12-14 12:38:54 +0530

    added some more text

--- hello world/hello.asm
+++ hello world/hello.asm
@@ -1,7 +1,8 @@
 section .data
-    text db "hello, world", 10
+    text db "hello, world", 11, 13, "second line", 10
     ; db stands for define bytes
     ; 10 is for new line , '\n'
+    ; 11 for vertical tab, 13 for carriage return
     ; text is the name for the memory location where this db is stored
 
 
@@ -28,8 +29,8 @@ _printingToScreen:
     ; for std_output the code is 1
     mov rsi, text
     ; the buffer location is text
-    mov rdx, 14
-    ; the length of buffer is 14
+    mov rdx, 27
+    ; the length of buffer is ~~14~~ 27
     syscall
     ; we call syscall with rax rdi rsi rdx i.e. 
     ; sys_write to std_output buffer at `text` of length 14
