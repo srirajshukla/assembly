781b78c861cccb76adc1548a6f7cf4f99dca880e
Gyanendra Shukla <srirajshukla@gmail.com>
2020-12-14 12:06:35 +0530

    Modified the program to use subroutine

--- hello world/hello.asm
+++ hello world/hello.asm
@@ -9,6 +9,18 @@ section .text
     global _start
 
 _start:
+    call _printingToScreen
+
+    mov rax, 60
+    ; 60 is code for sys_exit
+    mov rdi, 0
+    ; 0 is the return code
+    syscall
+    ; sys_exit(0)
+
+    ; registers are part of processor that temporarily holds memory
+
+_printingToScreen:
     ; we store the syscall id in rax register and arguments in corresponding registers
     mov rax, 1
     ; syscall code for sys_write is 1
@@ -22,11 +34,4 @@ _start:
     ; we call syscall with rax rdi rsi rdx i.e. 
     ; sys_write to std_output buffer at `text` of length 14
 
-    mov rax, 60
-    ; 60 is code for sys_exit
-    mov rdi, 0
-    ; 0 is the return code
-    syscall
-    ; sys_exit(0)
-
-    ; registers are part of processor that temporarily holds memory
+    ret
